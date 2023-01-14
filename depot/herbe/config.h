107d0d4af80caf833deda353dab5feb4650a5146
Gatlin Cheyenne Johnson <gatlin@niltag.net>
2023-01-13 19:50:20 -0600

    inter -> inter display; some of my fzf scripts look better now

--- depot/herbe/config.h
+++ depot/herbe/config.h
@@ -2,7 +2,7 @@ static const char *background_color = "#063409";
 static const char *border_color = "#00cc7e";
 
 static const char *font_color = "#f0fdf0";
-static const char *font_pattern = "Inter medium:size=18";
+static const char *font_pattern = "Inter Display Medium:size=18";
 static const unsigned line_spacing = 5;
 static const unsigned int padding = 15;
 
